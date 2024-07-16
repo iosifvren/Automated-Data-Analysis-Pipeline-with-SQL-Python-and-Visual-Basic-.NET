420689dbff41b1858eca1b743cff0cb5805b52dd
Iosif-Alexandru Vrencean <132339631+iosifvren@users.noreply.github.com>
2024-07-16 10:23:33 +0300

    Add files via upload

---
+++ Sub CreatePivotTable ().vb
@@ -0,0 +1,38 @@
+Sub CreatePivotTable()
+    Dim PSheet As Worksheet
+    Dim DSheet As Worksheet
+    Dim PCache As PivotCache
+    Dim PTable As PivotTable
+    Dim PRange As Range
+    Dim LastRow As Long
+    Dim LastCol As Long
+
+    Set DSheet = Worksheets("Data")
+
+    LastRow = DSheet.Cells(DSheet.Rows.Count, 1).End(xlUp).Row
+    LastCol = DSheet.Cells(1, DSheet.Columns.Count).End(xlToLeft).Column
+    Set PRange = DSheet.Cells(1, 1).Resize(LastRow, LastCol)
+
+    Set PCache = ActiveWorkbook.PivotCaches.Create( _
+        SourceType:=xlDatabase, _
+        SourceData:=PRange)
+
+    Set PSheet = Worksheets.Add
+
+    Set PTable = PCache.CreatePivotTable( _
+        TableDestination:=PSheet.Cells(1, 1), _
+        TableName:="PivotTable1")
+
+    With PTable
+    End With
+
+    With PTable
+        .PivotFields("Department").Orientation = xlRowField
+        
+    With .PivotFields("Employee ID")
+            .Orientation = xlDataField
+            .Function = xlCount
+            .Name = "Count of Employee ID"
+    End With
+End With
+End Sub
---
+++ extract data as xlsx.py
@@ -0,0 +1,3 @@
+import pandas as pd
+
+df.to_excel('File.xlsx', index=False)
